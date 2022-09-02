fa2c71f42e66463d5686a8725d8cde8b5f0f2195
Abdur-Rahman-Argon <106532958+Abdur-Rahman-Argon@users.noreply.github.com>
2022-09-02 16:15:05 +0600

    log in page design

--- src/components/Pages/LogIn/Login.js
+++ src/components/Pages/LogIn/Login.js
@@ -3,11 +3,78 @@ import SocialLogin from "./SocialLogin";
 
 const Login = () => {
   return (
-    <>
-      <div>
-        <SocialLogin></SocialLogin>
+    <div className=" flex flex-col sm:flex-row justify-center  px-10 sm:px-32 lg:px-10 ">
+      <div className="lg:flex-1 lg:flex hidden">
+        <img
+          src="https://www.cssscript.com/demo/login-page-template/img/bg.svg"
+          alt=""
+          className=" lg:w-11/12"
+        />
       </div>
-    </>
+      <div className="flex-1 mt-16 text-center">
+        <div className=" w-full md:w-[500px] lg:w-[350px] mx-auto">
+          <div>
+            <h2 class=" mb-8 text-3xl font-bold ">Please Login</h2>
+          </div>
+
+          <form className=" ">
+            <div class=" my-6">
+              <div class="div relative">
+                <input
+                  type="text"
+                  placeholder="User Email"
+                  class=" pl-10 py-1 text-gray-500 text-[16px] font-semibold input-bordered border-gray-400 border-b-2 focus:outline-none focus:border-b-success w-full"
+                  required
+                />
+                <div class=" absolute top-2 left-3">
+                  <i class="fas fa-user text-[16px]"></i>
+                </div>
+              </div>
+            </div>
+            <div class=" mt-5 mb-5">
+              <div class="div relative">
+                <input
+                  type="password"
+                  placeholder="User Email"
+                  class=" pl-10 py-1 text-gray-500 text-[16px] font-semibold input-bordered  border-gray-400 border-b-2 focus:outline-none focus:border-b-success w-full"
+                  required
+                />
+                <div class=" absolute top-2 left-3">
+                  <i class="fas fa-lock text-[16px]"></i>
+                </div>
+              </div>
+            </div>
+
+            <div class="my-3 text-right font-medium text-gray-400">
+              <a href="#">Forgot Password?</a>
+            </div>
+
+            <div>
+              <input
+                type="submit"
+                class="btn btn-success w-full text-white font-bold text-lg rounded-full my-3"
+                value="Login"
+              />
+            </div>
+          </form>
+
+          <div className=" my-3 font-semibold">
+            <p>
+              Not Register
+              <span className=" my-3 font-bold mx-1 text-success cursor-pointer">
+                Register Now
+              </span>
+            </p>
+          </div>
+
+          <div class="divider my-5 font-semibold text-gray-400">OR</div>
+
+          <div>
+            <SocialLogin></SocialLogin>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
--- src/components/Pages/LogIn/SocialLogin.js
+++ src/components/Pages/LogIn/SocialLogin.js
@@ -1,5 +1,9 @@
 import React from "react";
 import auth from "../../../firebase.init";
+import google from "./../../../images/google.png";
+import facebook from "./../../../images/facebook.png";
+import github from "./../../../images/github.png";
+import twitter from "./../../../images/twitter.png";
 // import { useSignInWithGoogle } from "firebase/auth";
 import { useAuthState, useSignInWithGoogle } from "react-firebase-hooks/auth";
 
@@ -12,15 +16,26 @@ const SocialLogin = () => {
   //   }
 
   return (
-    <div className=" text-center">
-      <button
-        onClick={() => signInWithGoogle()}
-        className=" btn btn-success px-10 my-10"
-      >
-        log in with Google
+    <div className=" text-center flex items-center justify-around my-8">
+      <button onClick={() => signInWithGoogle()}>
+        <img src={google} alt="" className=" w-8" />{" "}
+      </button>
+
+      <button onClick={""} className=" ">
+        <img src={facebook} alt="" className=" w-8" />{" "}
+      </button>
+
+      <button onClick={""} className=" ">
+        <img src={twitter} alt="" className=" w-8" />{" "}
+      </button>
+
+      <button onClick={""} className=" ">
+        <img src={github} alt="" className=" w-8" />{" "}
       </button>
     </div>
   );
 };
 
 export default SocialLogin;
+
+//  className="btn btn-success text-black w-full font-bold text-[16px] rounded-full my-5 flex items-center justify-around"  <span>Login with Google</span>
